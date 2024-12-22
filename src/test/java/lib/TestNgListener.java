a4ba2cee6ed1966782b73c1f4e7886cccadd502f
Brendan Leighton <br3ndan.l8n@gmail.com>
2024-12-21 20:06:13 -0500

    refactor(testng listener): add screenshot to listener and improve format

--- src/test/java/lib/TestNgListener.java
+++ src/test/java/lib/TestNgListener.java
@@ -4,7 +4,88 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+/**
+ * TestNG Listener class to log test results
+ */
 public class TestNgListener implements ITestListener {
+
+	String indent = "\t";
+	String testListStyle = "> ";
+	String testPropertyListStyle = "\t- ";
+	String startTestsMarker = "\n**********\n";
+	String finishTestsMarker = "\n**********\n";
+	StringBuilder testOutput = new StringBuilder();
+
+	/**
+	 * Print a formated list item for a test property. A test property could be the
+	 * test's status, filename for a screenshot, etc.
+	 * 
+	 * @param text the text to print
+	 */
+	private void printTestPropertyListItem(String text) {
+		System.out.println(this.testPropertyListStyle + text);
+	}
+
+	/**
+	 * Print the status of the test result. Converts the status integer to a string.
+	 * 
+	 * @param result ITestResult object
+	 */
+	private void printResultStatus(ITestResult result) {
+		this.printTestPropertyListItem("status: " + this.getStatusAsString(result.getStatus()));
+	}
+
+	/**
+	 * Get the status of the test result as a string.
+	 * 
+	 * @param status The status integer
+	 * @return The status as a string
+	 * @example getStatusAsString(ITestResult.getStatus());
+	 */
+	private String getStatusAsString(int status) {
+		String statusAsString;
+
+		// Convert the status integer to a string
+		switch (status) {
+			case -1:
+				statusAsString = "CREATED";
+				break;
+			case 1:
+				statusAsString = "SUCCESS";
+				break;
+			case 2:
+				statusAsString = "FAILURE";
+				break;
+			case 3:
+				statusAsString = "SKIP";
+				break;
+			case 4:
+				statusAsString = "SUCCESS_PERCENTAGE_FAILURE";
+				break;
+			case 16:
+				statusAsString = "STARTED";
+				break;
+			default:
+				statusAsString = "UNKNOWN";
+				break;
+		}
+
+		return statusAsString;
+	}
+
+	/**
+	 * Handle taking a screenshot of the current page. Take screenshot and log
+	 * filename it was saved as.
+	 * 
+	 * @param filename
+	 */
+	private void handleScreenshot(ITestResult result) {
+		this.printTestPropertyListItem(
+				Drivers.screenshot(
+						result.getName(), this.getStatusAsString(
+								result.getStatus())));
+	}
+
 	/**
 	 * Invoked each time before a test will be invoked. The <code>ITestResult</code>
 	 * is only partially
@@ -15,7 +96,9 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onTestStart(ITestResult result) {
-		System.out.println("STARTED TEST: " + result);
+		// page_tests.TDashboard.PageLoads -> remove the "page_tests." part
+		String qualifiedName = result.getMethod().getQualifiedName().substring(11);
+		System.out.println(this.testListStyle + qualifiedName);
 		ITestListener.super.onTestStart(result);
 	}
 
@@ -28,7 +111,7 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("PASSED TEST: " + result);
+		this.printResultStatus(result);
 		ITestListener.super.onTestSuccess(result);
 	}
 
@@ -41,7 +124,8 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onTestFailure(ITestResult result) {
-		System.out.println("FAILED TEST: " + result);
+		this.handleScreenshot(result);
+		this.printResultStatus(result);
 		ITestListener.super.onTestFailure(result);
 	}
 
@@ -54,7 +138,8 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		System.out.println("SKIPPED TEST: " + result);
+		this.handleScreenshot(result);
+		this.printResultStatus(result);
 		ITestListener.super.onTestSkipped(result);
 	}
 
@@ -69,7 +154,8 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		System.out.println("FAILED TEST WITHIN SUCCESS PERCENTAGE: " + result);
+		this.handleScreenshot(result);
+		this.printResultStatus(result);
 		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
 	}
 
@@ -81,7 +167,8 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onTestFailedWithTimeout(ITestResult result) {
-		System.out.println("FAILED TEST WITH TIMEOUT: " + result);
+		this.handleScreenshot(result);
+		this.printResultStatus(result);
 		ITestListener.super.onTestFailedWithTimeout(result);
 	}
 
@@ -94,7 +181,7 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onStart(ITestContext context) {
-		System.out.println("STARTING TESTS: " + context);
+		System.out.println(this.startTestsMarker + "STARTING TESTS: " + context);
 		ITestListener.super.onStart(context);
 	}
 
@@ -107,7 +194,7 @@ public class TestNgListener implements ITestListener {
 	 */
 	@Override
 	public void onFinish(ITestContext context) {
-		System.out.println("FINISHED TESTS: " + context);
+		System.out.println("FINISHED TESTS: " + context + this.finishTestsMarker);
 		ITestListener.super.onFinish(context);
 	}
 }
